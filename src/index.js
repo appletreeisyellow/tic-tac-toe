5080cf69e9c4185d58bd0b7a41df7b031aa250f9
Chunchun <chunchun@Chunchuns-MacBook-Pro.local>
2017-09-12 16:49:07 -0700

    finished Board

--- src/index.js
+++ src/index.js
@@ -17,8 +17,11 @@ class ShoppingList extends React.Component {
     );
   }
 }
+
+// Example usage: <ShoppingList name="Mark" />
 */
 
+/*
 class Square extends React.Component {
 
   render() {
@@ -29,19 +32,35 @@ class Square extends React.Component {
     );
   }
 }
+*/
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
 
 class Board extends React.Component {
   constructor() {
     super();
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    if(calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -52,7 +71,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if(winner) {
+      status = 'Winner: ' + winner; 
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -101,4 +126,29 @@ ReactDOM.render(
 );
 
 
-// Example usage: <ShoppingList name="Mark" />
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+
+
+
+
+
