a07254e8e9248dc35d5cc0541747c89bbb070861
Chunchun (Vivo) <yccvivo@gmail.com>
2021-09-16 14:41:39 -0700

    refactor: rewrite Board to use two loops to make the squares

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,29 +5,41 @@ import "../index.css";
 class Board extends React.Component {
   renderSquare(i) {
     return <Square
+      key={i}
       value={this.props.squares[i]}
       onClick={() => this.props.onClick(i)}
     />;
   }
 
+  generateRow(rowNum, rowNumMax) {
+    let row = [];
+
+    for (let i = rowNum; i < rowNumMax; i++) {
+      row.push(this.renderSquare(i));
+    }
+    return row;
+  }
+
+  generateBoard(columns, rows) {
+    let board = [];
+
+    for (let i = 0; i < columns * rows; i++) {
+      // generate row at the beginning of each row
+      if (i % columns === 0) {
+        board.push(
+          <div className="board-row" key={i}>
+            {this.generateRow(i, i + columns)}
+          </div>
+        )
+      }
+    }
+    return board;
+  }
+
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {this.generateBoard(3, 3)}
       </div>
     );
   }
